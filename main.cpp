9cff6f841817bb1c1f1338c893c42c1b5588c6ba
reidlum <reidlum@hawaii.edu>
2022-02-22 17:19:02 -1000

    Updated with my personal information.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ int main() {
    std::cout << "Hello, University of Hawaii, College of Engineering!" << std::endl;
    std::cout << "Lab 06d - Hello CLion - EE 205 - Spr 2022" << std::endl;
    std::cout << std::endl;
-   std::cout << "My name is:  <<YOUR NAME HERE>>" << std::endl;
-   std::cout << "A fun fact about me is <<TELL ME HOW MUCH YOU LOVE CATS>>" << std::endl;
+   std::cout << "My name is:  Reid Lum" << std::endl;
+   std::cout << "A fun fact about me is I have a severe addiction to ice cream, like it's super bad and I don't know how to stop it." << std::endl;
 
    return 0;
 }
